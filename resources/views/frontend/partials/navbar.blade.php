547a98a95e02e83f1bc79081b539d4ff5fb15b70
wahhabmalik <malik.wahhab@gmail.com>
2020-11-12 17:52:26 +0500

    final mokeup

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -13,7 +13,7 @@
                         <img src="{{ url('/') }}/theme/img/icons/phone.png" alt="" class="">
                     </div>
                     <div class="hib-text">
-                        <h6>+546 990221 123</h6>
+                        <h6>775-737-1355</h6>
                         <p>contact@ensurazone.com</p>
                     </div>
                 </div>
@@ -22,8 +22,8 @@
                         <img src="{{ url('/') }}/theme/img/icons/map-marker.png" alt="" class="">
                     </div>
                     <div class="hib-text">
-                        <h6>Main Str, no 23</h6>
-                        <p>NY, New York PK 23589</p>
+                        <h6>401 Ryland St, Suite 100</h6>
+                        <p>Reno, NV 89502, US</p>
                     </div>
                 </div>
                 <button class="search-switch"><i class="fa fa-search"></i></button>
